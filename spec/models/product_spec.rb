776babeb7ff581a7af660886a57340dbdecca9dd
Garyn Carrillo <iwdev@Garyns-MacBook-Pro.local>
2021-06-25 15:09:49 -0500

    Fix unit test on model product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,22 +4,22 @@ RSpec.describe Product, type: :model do
   context 'validation' do
     it "ensure the presene of name" do
       product = Product.new(name: "", description: 'new product', price: 100000, weight: 100).save
-      expect(product).to eq (false)
+      expect(product).to eq(false)
     end
 
     it "ensure the presene of description" do
       product = Product.new(name: "name", description: '', price: 100000, weight: 100).save
-      expect(product).to eq (false)
+      expect(product).to eq(false)
     end
 
     it "ensure the presene of price" do
-      product = Product.new(name: "name", description: 'description', price: 0, weight: 100).save
-      expect(product).to eq (false)
+      product = Product.new(name: "name", description: 'description', weight: 100).save
+      expect(product).to eq(false)
     end
 
     it "ensure the presene of weight" do
-      product = Product.new(name: "name", description: 'description', price: 100000, weight: 0).save
-      expect(product).to eq (false)
+      product = Product.new(name: "name", description: 'description', price: 100000).save
+      expect(product).to eq(false)
     end
   end
 end
